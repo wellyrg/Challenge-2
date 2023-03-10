7ce3401621209ebba36e2db17b22c395b9e69625
wellyrgg <wellyrgg.18@gmail.com>
2023-03-10 14:05:23 +0700

    second-commit

--- main.go
+++ main.go
@@ -4,28 +4,26 @@ import "fmt"
 
 func main() {
 
-	for i := 0; i <=4; i++ {	
+	for i := 0; i <= 4; i++ {
 		if i < 5 {
-			fmt.Printf("Nilai i = %d\n", i )
+			fmt.Printf("Nilai i = %d\n", i)
 		}
 	}
-	
+
 	for j := 0; j <= 10; j++ {
 		if j < 5 {
 			fmt.Printf("Nilai j = %d\n", j)
-		}else if j == 5{ 
+		} else if j == 5 {
 			var unicode [13]int32 = [13]int32{'C', 9, 'A', 1, 'Ш', 5, 'A', 7, 'P', 12, 'B', 11, 'O'}
 			for k := 0; k < len(unicode); k++ {
-				if k % 2 == 0 {
-				fmt.Printf("character %#U start at byte position %d\n", unicode[k], k)
+				if k%2 == 0 {
+					fmt.Printf("character %#U start at byte position %d\n", unicode[k], k)
 				}
 			}
-		}else if j > 5 {
+		} else if j > 5 {
 			fmt.Printf("Nilai j = %d\n", j)
 		}
 
 	}
 
-
-	
 }
